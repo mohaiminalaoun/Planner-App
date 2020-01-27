07c82a6aa1295eeabcab6afe007b4d979ab59ab9
mohaiminalaoun <mohaimin.al.aoun@vanderbilt.edu>
2020-01-27 12:51:11 -0500

    Updates

---
+++ precache-manifest.b016056fd5db91c4b1218df1d3fcb19d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fdab07fded349db26139ed202b9e5d8",
+    "url": "/Planner-app/index.html"
+  },
+  {
+    "revision": "c28694e6b990c9741ee8",
+    "url": "/Planner-app/static/css/2.e0f36c20.chunk.css"
+  },
+  {
+    "revision": "ba67983d21d9595b2222",
+    "url": "/Planner-app/static/css/main.3d2f606a.chunk.css"
+  },
+  {
+    "revision": "c28694e6b990c9741ee8",
+    "url": "/Planner-app/static/js/2.716de4db.chunk.js"
+  },
+  {
+    "revision": "520eefb5f8587d8c071be1d0d6b654dc",
+    "url": "/Planner-app/static/js/2.716de4db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba67983d21d9595b2222",
+    "url": "/Planner-app/static/js/main.fa35dfab.chunk.js"
+  },
+  {
+    "revision": "521d234fb3f87aa9c6a7",
+    "url": "/Planner-app/static/js/runtime-main.5661c5a1.js"
+  },
+  {
+    "revision": "34f3f57e6cc37a84b9457942a5e0cb9b",
+    "url": "/Planner-app/static/media/menu.34f3f57e.svg"
+  },
+  {
+    "revision": "604de84e6008934748090fa18bdf12a6",
+    "url": "/Planner-app/static/media/sort.604de84e.svg"
+  },
+  {
+    "revision": "a36421a8af69c5c9c01f160d245944ef",
+    "url": "/Planner-app/static/media/trash.a36421a8.svg"
+  }
+]);
