16dd7c5ac3be000e3b03c3233b12982f1e7b7a50
mohaiminalaoun <mohaimin.al.aoun@vanderbilt.edu>
2020-02-06 15:58:28 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Planner-App/precache-manifest.4adf23eeef5754b17440f3623df27445.js"
+  "/Planner-App/precache-manifest.02b95e056d243493bbed9eee48439773.js"
 );
 
 self.addEventListener('message', (event) => {
