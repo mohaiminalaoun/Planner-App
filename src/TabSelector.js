f7e3cda28496018233f14bae63f5e177e4dc3c40
Mohaimin Al Aoun <mohaimin.al.aoun@vanderbilt.edu>
2020-05-18 15:48:47 -0400

    updates

--- src/TabSelector.js
+++ src/TabSelector.js
@@ -9,14 +9,14 @@ const TabSelector = ({ shouldShowDashboard, hideDashboard, showDashboard }) => {
         onClick={hideDashboard}
       >
         Tasks
-        <div class="selectorDiv"></div>
+        <div className="selectorDiv"></div>
       </Badge>
       <Badge
         className={"tabselector " + (shouldShowDashboard ? "selected" : "")}
         onClick={showDashboard}
       >
         Statistics
-        <div class="selectorDiv"></div>
+        <div className="selectorDiv"></div>
       </Badge>
     </>
   );
